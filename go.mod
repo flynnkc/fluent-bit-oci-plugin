4eef826847f13f19a70af2fe982e41fbe40fa3ad
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2025-03-18 12:25:18 -0400

    Register & Init

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/flynnkc/fluent-bit-oci-plugin
 
 go 1.23.3
+
+require (
+	github.com/fluent/fluent-bit-go v0.0.0-20230731091245-a7a013e2473c
+	github.com/flynnkc/go-oci-log-writer v0.0.0-20250317191456-13d43293942d
+	github.com/oracle/oci-go-sdk/v65 v65.87.0
+)
+
+require (
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sony/gobreaker v0.5.0 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,75 @@
 package main
 
+import (
+	"C"
+	"unsafe"
+
+	"log"
+	"strconv"
+
+	"github.com/fluent/fluent-bit-go/output"
+	ocilog "github.com/flynnkc/go-oci-log-writer"
+
+	"github.com/oracle/oci-go-sdk/v65/common/auth"
+)
+
+var (
+	writer  *ocilog.LogWriter
+	logType string = "OCI Fluent-Bit Plugin" // Identify logs as coming from plugin
+)
+
 func main() {}
+
+func FLBPluginRegister(def unsafe.Pointer) int {
+	return output.FLBPluginRegister(def, "oci-logging",
+		"Plugin for OCI Logging Service")
+}
+
+// FLBPluginInit is where we create instances stored in globals for setup
+func FLBPluginInit(plugin unsafe.Pointer) int {
+	p, err := auth.OkeWorkloadIdentityConfigurationProvider()
+	if err != nil {
+		log.Printf("unable to initialize workload identity provider: %s", err)
+		return output.FLB_ERROR
+	}
+
+	// Mandatory variables
+	logId := output.FLBPluginConfigKey(plugin, "log_id")
+	src := output.FLBPluginConfigKey(plugin, "log_source")
+
+	d := ocilog.LogWriterDetails{
+		Provider: p,
+		LogId:    &logId,
+		Type:     &logType,
+		Source:   &src,
+	}
+
+	// Optional variables
+	if subject := output.FLBPluginConfigKey(plugin, "subject"); subject != "" {
+		d.Subject = &subject
+	}
+	if buf := output.FLBPluginConfigKey(plugin, "buffer_size"); buf != "" {
+		bufSize, err := strconv.Atoi(buf)
+		if err == nil {
+			d.BufferSize = &bufSize
+		} else {
+			log.Printf("invalid buffer size %s, using default", buf)
+		}
+	}
+
+	writer, err = ocilog.New(d)
+	if err != nil {
+		log.Printf("unable to initialize workload identity provider: %s", err)
+		return output.FLB_ERROR
+	}
+
+	return output.FLB_OK
+}
+
+func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
+	return output.FLB_OK
+}
+
+func FLBPluginExit() int {
+	return output.FLB_OK
+}
